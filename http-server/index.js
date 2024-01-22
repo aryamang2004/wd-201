2b41d7b2d1ce390d764ba493847e42488898f3fb
Aryaman G <157365180+aryamang19@users.noreply.github.com>
2024-01-22 17:03:37 +0530

    Create index.js

---
+++ http-server/index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+
+let homeContent = '';
+let projectContent = '';
+let registrationContent = '';
+
+fs.readFile('registration.html', (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+fs.readFile('home.html', (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile('project.html', (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+let port = 3000;
+
+const args = require('minimist')(process.argv.slice(2), {
+  default: {
+    port: 3000,
+  },
+});
+
+port = args['port'];
+
+http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHeader(200, { 'Content-Type': 'text/html' });
+  switch (url) {
+    case '/project':
+      response.write(projectContent);
+      response.end();
+      break;
+    case '/registration':
+      response.write(registrationContent);
+      response.end();
+      break;
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+  }
+}).listen(port);
